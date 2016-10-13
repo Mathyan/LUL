bacce0bbe454a4fc8d3d8871daaac854d681b0a7
matij <matij@DESKTOP-7MRQ72T>
2016-10-13 14:08:34 +0200

    Add project files.

---
+++ LUL/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim answer As String
+        Console.WriteLine("Dali želite pozdrav?")
+        Console.WriteLine("Pritisnite Y za DA, N za ne")
+        'Test()
+        answer = Console.ReadLine()
+        If answer = "Y" Then
+            Console.WriteLine("Hello")
+        Else
+            Console.WriteLine("BYE")
+        End If
+
+
+    End Sub
+
+
+
+End Module
