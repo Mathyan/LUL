1c975732a6f22f1cbd5c1c449be39d96a326ac72
matij <matij@DESKTOP-7MRQ72T>
2016-10-13 14:27:47 +0200

    New Test

--- LUL/Module1.vb
+++ LUL/Module1.vb
@@ -1,19 +1,36 @@
 ﻿Module Module1
 
     Sub Main()
-        Dim answer As String
-        Console.WriteLine("Dali želite pozdrav?")
-        Console.WriteLine("Pritisnite Y za DA, N za ne")
-        'Test()
-        answer = Console.ReadLine()
-        If answer = "Y" Then
-            Console.WriteLine("Hello")
-        Else
-            Console.WriteLine("BYE")
-        End If
+
+        While Console.ReadLine() IsNot "Ne"
+            Test2()
+        End While
 
 
     End Sub
+    Sub Test2()
+        Dim answer As String
+
+        Test(1)
+        answer = Console.ReadLine()
+        If answer = "Y" Then
+            Test(2)
+        Else
+            Test(3)
+        End If
+    End Sub
+
+    Private Sub Test(v As Integer)
+        If v = 1 Then
+            Console.WriteLine("Dali želite pozdrav?")
+            Console.WriteLine("Pritisnite Y za DA, N za ne")
+        ElseIf v = 2 Then
+            Console.WriteLine("Hello")
+        ElseIf v = 3 Then
+            Console.WriteLine("BYE")
+        End If
+    End Sub
+
 
 
 
